64fc565609e1b236c17f3a141068e8424f265aef
unknown <massimo.bernava@gmail.com>
2022-03-30 21:19:04 +0100

    fix

--- tensorflow/lite/delegates/external/external_delegate.cc
+++ tensorflow/lite/delegates/external/external_delegate.cc
@@ -36,8 +36,7 @@ struct ExternalLib {
   // Open a given delegate library and load the create/destroy symbols
   bool load(const std::string library) {
 #if defined(_WIN32)
-    void* handle = SharedLibrary::LoadLibrary(
-        converter.from_bytes(library.c_str()).c_str());
+    void* handle = SharedLibrary::LoadLibrary(library.c_str());
 #else
     void* handle = SharedLibrary::LoadLibrary(library.c_str());
 #endif  // defined(_WIN32)
